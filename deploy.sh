ce23bd9ec83fba8e2f351e313a778e7caf27e927
chris <chris@chris-OptiPlex-980.(none)>
2014-01-10 22:53:18 +1100

    deploy script update

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,6 @@
+cp tiledemo.html /home/chris/generate_tiles/highcontrast/index.html 
+
 rsync -zva /home/chris/generate_tiles/highcontrast/ chrislee@wdev:/web/50018/htdocs/water/maps/highcontrast/
+
+cd /home/chris/generate_tiles/highcontrast/ 
+rm -rf *
